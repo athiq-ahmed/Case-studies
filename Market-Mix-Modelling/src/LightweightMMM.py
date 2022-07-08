b0f39b4a05ba5323522dc7eb1b395f9dad2e6d9d
Athiq <athiqahmed.ai@gmail.com>
2022-07-08 17:07:57 +0530

    adding

--- Market-Mix-Modelling/src/LightweightMMM.py
+++ Market-Mix-Modelling/src/LightweightMMM.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from lightweight_mmm import preprocessing, lightweight_mmm, plot, optimize_media
 import jax.numpy as jnp
+from sklearn.preprocessing import MaxAbsScaler
 from sklearn.metrics import mean_absolute_error
 from tabulate import tabulate
 
@@ -46,8 +47,41 @@ costs_raw = agg_data['Spend'].unstack();costs_raw.head()
 sales_raw = agg_data['Sales'].reset_index().groupby("Date").sum(); sales_raw.head()
 media_data_raw['Sales'] = sales_raw
 media_data_raw = media_data_raw.reset_index()
-media_data_raw.head()
+media_data_raw.head(10)
+
+media_data_raw['Date'].min()    # Timestamp('2021-10-17 00:00:00')
+media_data_raw['Date'].max()    # Timestamp('2022-01-11 00:00:00')
+media_data_raw['Date'].max() - media_data_raw['Date'].min()  # 86 days
+
 
 # MaxAbsScaler simply divides each column value by the maximal absolute value it can find on the column.
+media_data_raw.columns
+date_col = 'Date'
+response_col = 'Sales'
+
+regressor_cols = ['Brand 1 Ad Group 1', 'Brand 1 Ad Group 10', 'Brand 1 Ad Group 11', 'Brand 1 Ad Group 13', 'Brand 1 Ad Group 2', 'Brand 1 Ad Group 3',
+                 'Brand 1 Ad Group 4', 'Brand 1 Ad Group 5', 'Brand 1 Ad Group 6', 'Brand 1 Ad Group 7', 'Brand 1 Ad Group 8', 'Brand 1 Ad Group 9',
+                 'Brand 2 Ad Group 1', 'Brand 2 Ad Group 2', 'Brand 2 Ad Group 3', 'Brand 2 Ad Group 4', 'Brand 2 Ad Group 5', 'Brand 1 Ad Group 14', 
+                 'Brand 2 Ad Group 6']
 
 
+split_point = -28 # 28 days to end of data
+
+media_data_train = media_data_raw.iloc[:split_point].copy()
+media_data_test = media_data_raw.iloc[split_point:].copy()
+
+media_data_raw.shape    # (87, 21)
+media_data_train.shape  # (59, 21)
+media_data_test.shape   # (28, 21)
+
+media_data_train.head()
+media_data_test.head()
+
+scaler_x = MaxAbsScaler()
+media_data_train.loc[:, regressor_cols] = scaler_x.fit_transform(media_data_train.loc[:, regressor_cols])
+media_data_test.loc[:, regressor_cols] = scaler_x.transform(media_data_test.loc[:, regressor_cols])
+
+scaler_y = MaxAbsScaler()
+media_data_train.loc[:, response_col] = scaler_y.fit_transform(media_data_train.loc[:, response_col].values.reshape(-1,1))
+
+media_names = media_data_raw.columns
